89c55257008941c1888da7fe8b9ceff780619a04
Matt Stegall <mstegall928@gmail.com>
2017-01-17 18:55:55 -0500

    Functions, directory processing, initialization

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const fs = require('fs');
 const nopt = require('nopt');
 const path = require('path');
 let labelNumber = 0;
+let returnNumber = 0;
 
 function getParsedOptions() {
     const options = {
-        'file': path
+        'file': path,
+        'dir': path,
+        'no-init': Boolean
     }
     const shortHands = {
-        'f': ['--file']
+        'f': ['--file'],
+        'd': ['--dir'],
+        'n': ['--noInit']
     }
 
     return nopt(options, shortHands);
@@ -28,12 +33,12 @@ function newLabel() {
     return `l$${labelNumber++}`
 }
 
-function parseLine(line) {
+function parseLine(line, fileName) {
     const [command, arg1, arg2] = line.split(' ');
     switch (command) {
         case 'add':
             return [
-                '@SP',
+                '@SP // ADD',
                 'AM=M-1',
                 'D=M',
                 'A=A-1',
@@ -41,7 +46,7 @@ function parseLine(line) {
             ].join('\n');
         case 'sub':
             return [
-                '@SP',
+                '@SP // SUB',
                 'AM=M-1',
                 'D=M',
                 'A=A-1',
@@ -151,7 +156,7 @@ function parseLine(line) {
             switch (arg1) {
                 case 'local':
                     return [
-                        '@LCL',
+                        '@LCL // POP',
                         'D=M',
                         `@${arg2}`,
                         'D=D+A',
@@ -166,7 +171,7 @@ function parseLine(line) {
                     ].join('\n');
                 case 'argument':
                     return [
-                        '@ARG',
+                        '@ARG // POP',
                         'D=M',
                         `@${arg2}`,
                         'D=D+A',
@@ -181,7 +186,7 @@ function parseLine(line) {
                     ].join('\n');
                 case 'this':
                     return [
-                        '@THIS',
+                        '@THIS // POP',
                         'D=M',
                         `@${arg2}`,
                         'D=D+A',
@@ -196,7 +201,7 @@ function parseLine(line) {
                     ].join('\n');
                 case 'that':
                     return [
-                        '@THAT',
+                        '@THAT // POP',
                         'D=M',
                         `@${arg2}`,
                         'D=D+A',
@@ -211,7 +216,7 @@ function parseLine(line) {
                     ].join('\n');
                 case 'temp':
                     return [
-                        '@SP',
+                        '@SP // POP',
                         'AM=M-1',
                         'D=M',
                         `@${+arg2 + 5}`,
@@ -219,15 +224,15 @@ function parseLine(line) {
                     ].join('\n');
                 case 'static':
                     return [
-                        '@SP',
+                        '@SP // POP',
                         'AM=M-1',
                         'D=M',
-                        `@${+arg2 + 16}`,
+                        `@${fileName}.${arg2}`,
                         'M=D'
                     ].join('\n');
                 case 'pointer':
                     return [
-                        '@SP',
+                        '@SP // POP',
                         'AM=M-1',
                         'D=M',
                         `@${+arg2 + 3}`,
@@ -239,7 +244,7 @@ function parseLine(line) {
             switch (arg1) {
                 case 'constant': {
                     return [
-                        `@${arg2}`,
+                        `@${arg2} // PUSH`,
                         'D=A',
                         '@SP',
                         'M=M+1',
@@ -249,7 +254,7 @@ function parseLine(line) {
                 }
                 case 'local':
                     return [
-                        '@LCL',
+                        '@LCL // PUSH',
                         'D=M',
                         `@${arg2}`,
                         'A=D+A',
@@ -261,7 +266,7 @@ function parseLine(line) {
                     ].join('\n');
                 case 'argument':
                     return [
-                        '@ARG',
+                        '@ARG // PUSH',
                         'D=M',
                         `@${arg2}`,
                         'A=D+A',
@@ -273,7 +278,7 @@ function parseLine(line) {
                     ].join('\n');
                 case 'this':
                     return [
-                        '@THIS',
+                        '@THIS // PUSH',
                         'D=M',
                         `@${arg2}`,
                         'A=D+A',
@@ -285,7 +290,7 @@ function parseLine(line) {
                     ].join('\n');
                 case 'that':
                     return [
-                        '@THAT',
+                        '@THAT // PUSH',
                         'D=M',
                         `@${arg2}`,
                         'A=D+A',
@@ -297,7 +302,7 @@ function parseLine(line) {
                     ].join('\n');
                 case 'temp':
                     return [
-                        `@${+arg2 + 5}`,
+                        `@${+arg2 + 5} // PUSH`,
                         'D=M',
                         '@SP',
                         'M=M+1',
@@ -306,7 +311,7 @@ function parseLine(line) {
                     ].join('\n');
                 case 'static':
                     return [
-                        `@${+arg2 + 16}`,
+                        `@${fileName}.${arg2} // PUSH`,
                         'D=M',
                         '@SP',
                         'M=M+1',
@@ -315,7 +320,7 @@ function parseLine(line) {
                     ].join('\n');
                 case 'pointer':
                     return [
-                        `@${+arg2 + 3}`,
+                        `@${+arg2 + 3} // PUSH`,
                         'D=M',
                         '@SP',
                         'M=M+1',
@@ -327,22 +332,147 @@ function parseLine(line) {
             return `(${arg1})`;
         case 'goto':
             return [
-                `@${arg1}`,
+                `@${arg1} // goto ${arg1}`,
                 '0;JMP',
             ].join('\n');
         case 'if-goto':
             return [
-                '@SP',
+                '@SP // if-goto',
                 'AM=M-1',
                 'D=M',
                 `@${arg1}`,
                 'D;JNE'
             ].join('\n');
+        case 'call':
+            const returnLabel = `RETURN${returnNumber++}`
+            return [
+                `//${line}`,
+                `@${returnLabel}`,
+                'D=A',
+                '@SP',
+                'M=M+1',
+                'A=M-1',
+                'M=D',
+                parseLine('push temp -4'),
+                parseLine('push temp -3'),
+                parseLine('push temp -2'),
+                parseLine('push temp -1'),
+                `@${+arg2 + 5}`,
+                'D=A',
+                '@SP',
+                'D=M-D',
+                '@ARG',
+                'M=D',
+                '@SP',
+                'D=M',
+                '@LCL',
+                'M=D',
+                `@${arg1}`,
+                '0;JMP',
+                `(${returnLabel})`
+            ].join('\n');
+        case 'function':
+            const lines = [
+                `//${line}`,
+                `(${arg1})`,
+                '@SP',
+                'A=M',
+            ];
+
+            for (var i = 0; i < arg2; i++) {
+                lines.push('M=0',
+                    'A=A+1');
+            }
+
+            lines.push(...[
+                `@${arg2}`,
+                'D=A',
+                '@SP',
+                'M=D+M'
+            ])
+            return lines.join('\n');
+        case 'return':
+            return [
+                `@LCL //${line}`,
+                'D=M-1',
+                '@R13',
+                'M=D',
+                '@4',
+                'A=D-A',
+                'D=M',
+                '@R14',
+                'M=D',
+                '@SP // POP ARG ',
+                'A=M-1',
+                'D=M',
+                '@ARG',
+                'A=M',
+                'M=D',
+                '@ARG // SP = ARG + 1',
+                'D=M+1',
+                '@SP',
+                'M=D',
+                '@R13 // RESTORE THAT',
+                'A=M',
+                'D=M',
+                '@THAT',
+                'M=D',
+                '@R13 // RESTORE THIS',
+                'AM=M-1',
+                'D=M',
+                '@THIS',
+                'M=D',
+                '@R13 // RESTORE ARG',
+                'AM=M-1',
+                'D=M',
+                '@ARG',
+                'M=D',
+                '@R13 // RESTORE LCL',
+                'AM=M-1',
+                'D=M',
+                '@LCL',
+                'M=D',
+                '@R14 // GOTO RETURN',
+                'A=M',
+                '0;JMP',
+            ].join('\n');
     }
 }
 
-const {file: fileName} = getParsedOptions();
-const lines = fs.readFileSync(fileName, 'utf8').split(/\n|\r\n/);
-const noWhitespaceLines = removeWhitespace(lines);
-const commands = noWhitespaceLines.map(parseLine);
-fs.writeFileSync(`${fileName.split('.')[0]}.asm`, commands.join("\n"));
+function parseFile(filePath) {
+    const fileName = path.parse(filePath).name
+    const lines = fs.readFileSync(filePath, 'utf8').split(/\n|\r\n/);
+    const noWhitespaceLines = removeWhitespace(lines);
+    return noWhitespaceLines.map(line => parseLine(line, fileName)).join('\n');
+}
+
+const {file: fileName, dir: folderName, noInit} = getParsedOptions();
+
+const commandSets = [];
+
+if (!noInit) {
+    const initCode = [
+        '@256',
+        'D=A',
+        '@SP',
+        'M=D',
+        parseLine('call Sys.init 0'),
+    ].join('\n');
+
+    commandSets.push(initCode)
+}
+
+if (fileName) {
+    commandSets.push(parseFile(fileName));
+}
+if (folderName) {
+    files = fs.readdirSync(folderName);
+    files.forEach(fileName => {
+        if (path.extname(fileName) === '.vm') commandSets.push(parseFile(path.join(folderName, fileName)));
+    })
+}
+
+const outFileName = (fileName && fileName.split('.')[0]) || folderName;
+
+
+fs.writeFileSync(`${outFileName}.asm`, commandSets.join("\n"));
